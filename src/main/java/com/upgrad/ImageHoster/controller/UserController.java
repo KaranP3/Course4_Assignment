45e52e3cad59ed75ab6ed859b2cb74cff8fef6f0
KaranP3 <karanpillai93@gmail.com>
2018-09-14 17:55:01 +0530

    Changes made to preserve edit and delete functionalities
    Changes made to ImageController.java, ImageManager.java, ImageService.java, ImageServiceImpl.java, image.html and edit.html so that edit and delete functionalities are still working after changes made during fix of image upload issues. The edit and delete functionalities now also retrieve the image by id instead of title and these functionalities will now work on images that share the same title as given in image upload issues.

--- src/main/java/com/upgrad/ImageHoster/controller/UserController.java
+++ src/main/java/com/upgrad/ImageHoster/controller/UserController.java
@@ -71,22 +71,23 @@ public class UserController {
 
         if (userService.getByName(username) == null) {
 
-            // We'll first assign a default photo to the user
-            ProfilePhoto photo = new ProfilePhoto();
-            profilePhotoService.save(photo);
+                // We'll first assign a default photo to the user
+                ProfilePhoto photo = new ProfilePhoto();
+                profilePhotoService.save(photo);
 
-            // it is good security practice to store the hash version of the password
-            // in the database. Therefore, if your a hacker gains access to your
-            // database, the hacker cannot see the password for your users
-            String passwordHash = hashPassword(password);
-            User user = new User(username, passwordHash, photo);
-            userService.register(user);
+                // it is good security practice to store the hash version of the password
+                // in the database. Therefore, if your a hacker gains access to your
+                // database, the hacker cannot see the password for your users
+                String passwordHash = hashPassword(password);
+                User user = new User(username, passwordHash, photo);
+                userService.register(user);
 
-            // We want to create an "currUser" attribute in the HTTP session, and store the user
-            // as the attribute's value to signify that the user has logged in
-            session.setAttribute("currUser", user);
+                // We want to create an "currUser" attribute in the HTTP session, and store the user
+                // as the attribute's value to signify that the user has logged in
+                session.setAttribute("currUser", user);
 
-            return "redirect:/";
+                return "redirect:/";
+
         } else {
 
             errors.put("username", "username has been registered");
